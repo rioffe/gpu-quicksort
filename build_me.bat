ad4cbe06a31306addc1d1bbf2338647cf736a4f6
Robert Ioffe <robert.ioffe@intel.com>
2020-03-23 14:35:18 -0700

    added build_me.bat to build on Windows from Intel oneAPI Tools shell and run four versions with various sizes

---
+++ build_me.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+for %%a in (GPU-Quicksort_OpenCL_1.2 GPU-Quicksort_OpenCL_2.0 GPU-Quicksort_SYCL GPU-Quicksort_Full_SYCL) do (
+  echo --------------- Algorithm %%a ----------------
+  cd %%a
+  echo ... Building ...
+  dpcpp-cl /MD /GX /D_CRT_SECURE_NO_WARNINGS -o Quicksort.exe QuicksortMain.cpp OpenCL.lib
+  echo ... Done ...
+  set "nmbr=0"
+  for %%c in (256 512 1024 2048 4096 8192 16384) do call :run_loops %%c
+  cd ..
+)
+goto :eof
+
+:run_loops
+for %%i in (1 2 3) do (
+  echo -------------- Trial %%i ---- %1 %1 ----------
+  .\Quicksort.exe 10 gpu intel %1 %1 show_cl
+)
+set /A D=2*%1
+for %%i in (1 2 3) do (
+  echo -------------- Trial %%i ----- %D% %1 ---------
+  .\Quicksort.exe 10 gpu intel %D% %1 show_cl
+)
+
+goto :eof
